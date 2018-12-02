b488768cb068a219763e6373516127130fc92ffb
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2018-12-02 18:06:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -112,3 +112,21 @@ client.on('message', async msg => {
   msg.channel.send({file: { attachment:body.message, name: 'clyde.png'}}).then(()=> { searchMessage.delete(); msg.channel.stopTyping(); });
 };
 });
+       var prefix = "y!";
+    client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  if (command == "spam") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR**');
+ 
+ 
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
