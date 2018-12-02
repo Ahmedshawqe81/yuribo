294213d54fbbc8ef22a48b22491320a30ab0f32f
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2018-12-02 17:38:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.user.setGame(``,'https://www.twitch.tv/tarikrs');                        
 });          
 
 client.on('message', msg => {
-	var prefix = "$";
+	var prefix = "y!";
   if (msg.author.bot) return;
   if (!msg.content.startsWith(prefix)) return;
   let command = msg.content.split(" ")[0];
@@ -44,48 +44,5 @@ client.on('message', msg => {
         msg.channel.send("```Cleard: " + textxt + "\n Messages```").then(m => m.delete(3000));
         }    
     }
-}
-});
-client.on('message', message => {
-var prefix = "a!" // البريفكس
-     if (message.author.bot) return;
-if (message.content.startsWith(prefix + "time")) { // الامر
-    let uptime = client.uptime;
-
-    let days = 0;
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    let notCompleted = true;
-
-    while (notCompleted) {
-
-        if (uptime >= 8.64e+7) {
-
-            days++;
-            uptime -= 8.64e+7;
-
-        } else if (uptime >= 3.6e+6) {
-
-            hours++;
-            uptime -= 3.6e+6;
-
-        } else if (uptime >= 60000) {
-
-            minutes++;
-            uptime -= 60000;
-
-        } else if (uptime >= 1000) {
-            seconds++;
-            uptime -= 1000;
-
-        }
-
-        if (uptime < 1000)  notCompleted = false;
-
-    }
-
-    message.channel.send("" +${days} days, ${hours} hrs, ${minutes} , ${seconds} sec+ "");
-
 }
 });
